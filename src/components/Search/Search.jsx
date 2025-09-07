894c27376c625cea1981c6dfa7c2f327acb322dc
Doron Pela <devronpela@gmail.com>
2025-09-07 13:19:47 +0100

    Implemented background animation in Search page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,28 @@
+import aos from "aos";
+import { useEffect } from "react";
 import style from "./Search.module.css";
 
+
 export default function Search() {
+  useEffect(() => {
+    aos.init({ duration: 500 });
+  }, []);
+
   return (
     <section className={style["search"]}>
       <div className={style["search-container"]}>
         <div className={style["background"]}>
-            <div ></div>
-            <div ></div>
-            <div ></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
+        <div className={style["left-blur"]}></div>
+        <div className={style["right-blur"]}></div>
+        <div className={style["sub-left-blur"]}></div>
+        <div className={style["sub-right-blur"]}></div>
         <div className={style["search-bar-container"]}>
           <input
+            data-aos={"fade-up"}
             placeholder={"Search a game"}
             className={style["search-bar"]}
           />
