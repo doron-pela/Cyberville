32fa4e578cf207ea2f8d13ff3f19f850715d5f63
Doron Pela <devronpela@gmail.com>
2025-09-09 00:40:05 +0000

    Re-built video element as component and implemented video carousel in Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ReactPlayer from "react-player";
 import style from './Hero.module.css';
 import mutedIcon from "../../assets/audio-volume-muted-symbolic-svgrepo-com.svg";
 import unmutedIcon from "../../assets/unmute-svgrepo-com.svg";
+import nextArrow from "../../assets/arrow-next-small-svgrepo-com.svg";
+import prevArrow from "../../assets/arrow-prev-small-svgrepo-com.svg";
 import YouTube  from "react-youtube";
+
+// import src2 from './video-1.mp4';
+
+import Video from "../Video/Video.jsx";
 
 export default function Hero({welcomed, setWelcomed}) {
 
+    const [index, setIndex] = useState(0);
     const [muted, setMuted] = useState(true);
+    
+    const videos = ["https://cyberville-hero-video.sfo3.digitaloceanspaces.com/video-2.mp4"];
 
-    useEffect(()=>{
-      async function fetchData(){
-        const unWrapped = await fetch().then((res) => res.json());
-        return unWrapped;
-      }
+    // useEffect(()=>{
+    //   async function fetchData(){
+    //     const unWrapped = await fetch().then((res) => res.json());
+    //     return unWrapped;
+    //   }
 
-      const data = fetchData();
+    //   const data = fetchData();
       
-      console.log(data);
-    }, [])
+    //   console.log(data);
+    // }, [])
 
     function unMute(e){
         if(e.target.matches('video')){
@@ -32,6 +41,20 @@ export default function Hero({welcomed, setWelcomed}) {
       setWelcomed(true);
     }
 
+    function incrementIndex(){
+      index === videos.length-1? setIndex(0) : setIndex((index) => index+1);
+    }
+
+    function decrementIndex() {
+      index === 0? setIndex(videos.length-1): setIndex((index) => index -1);
+    }
+
+    
+      console.log(index);
+
+    
+
+
     return (
       <section
         className={style.hero}
@@ -39,41 +62,45 @@ export default function Hero({welcomed, setWelcomed}) {
         onScroll={(e) => unMute(e)}
         onClick={(e) => unMute(e)}
       >
-        
         <img
           src={muted ? mutedIcon : unmutedIcon}
           className={style.mutedIcon}
           onClick={() => setMuted(!muted)}
         />
 
-        <video
-          className={style.player}
-          autoPlay
-          //   controls
-          muted={muted}
+        <Video key={videos[index]} src={videos[index]} muted={muted} />
+
+        <aside
+          className={[
+            `${style["hero-left"]} ${welcomed ? style["welcomed"] : ""}`,
+          ]}
         >
-          <source src={"https://cyberville-hero-video.sfo3.digitaloceanspaces.com/video-2.mp4"} type="video/webm" />
-          <source src={"https://cyberville-hero-video.sfo3.digitaloceanspaces.com/video-2.mp4"} type="video/mp4" />
+          <h1> Cyberville </h1>
           <p>
-            Your browser doesn't support HTML video. Here is a
-            <a href={"https://cyberville-hero-video.sfo3.digitaloceanspaces.com/video-2.mp4"} download="myVideo.mp4">
-              link to the video
-            </a>{" "}
-            instead.
+            Your gateway to the ultimate gameverse. Discover, buy, and play the
+            newest games the moment they drop, all in one city of pixels.
           </p>
-        </video>
 
-        
-        <aside className={[`${style['hero-left']} ${welcomed? style["welcomed"] : ''}`]}>
-            <h1> Cyberville </h1>
-            <p>
-              Your gateway to the ultimate gameverse. Discover, buy, and play the
-              newest games the moment they drop, all in one city of pixels.
-            </p>
-
-            <button onClick={()=>clearSiteCache()}className={style["pulse"]}>Clear Site cache</button>
+          <button onClick={() => clearSiteCache()} className={style["pulse"]}>
+            Clear Site cache
+          </button>
         </aside>
-        
+
+        <div className={style["arrows"]}>
+          <div
+            onClick={() => decrementIndex()}
+            className={`${style["arrow"]} ${style["prev"]}`}
+          >
+            <img src={prevArrow} alt="prev-arrow" />
+          </div>
+
+          <div
+            onClick={() => incrementIndex()}
+            className={`${style["arrow"]} ${style["next"]}`}
+          >
+            <img src={nextArrow} alt="next-arrow" />
+          </div>
+        </div>
 
         <aside className={style["hero-right"]}>
           {<button className={style.btn}>Get now</button>}
---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,24 @@
+import style from './Video.module.css';
+
+
+export default function Video({src, muted}){
+
+    return(
+        <video
+          className={style.player}
+          autoPlay
+          //   controls
+          muted={muted}
+        >
+          <source src={src} type="video/webm" />
+          <source src={src} type="video/mp4" />
+          <p>
+            Your browser doesn't support HTML video. Here is a
+            <a href={src} download="myVideo.mp4">
+              link to the video
+            </a>{" "}
+            instead.
+          </p>
+        </video>
+    );
+}
